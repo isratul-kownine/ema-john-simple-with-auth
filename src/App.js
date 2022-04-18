d3f715d92937ab935529d9eb6da3f37b90aac1d1
isratul-kownine <isratulkownine222@gmail.com>
2022-04-18 18:58:59 +0600

    implemented login signup requireAuth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
+import { SignInMethod } from 'firebase/auth';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
 import Header from './components/Header/Header';
 import Inventory from './components/Inventory/Inventory';
+import LogIn from './components/LogIn/LogIn';
 import Orders from './components/Orders/Orders';
+import RequireAuth from './components/RequireAuth/RequireAuth';
 import Shop from './components/Shop/Shop';
+import SignUp from './components/SignUp/SignUp';
 
 function App() {
   return (
@@ -15,8 +19,13 @@ function App() {
         <Route path='/' element={<Shop></Shop>}></Route>
         <Route path='/shop' element={<Shop></Shop>}></Route>
         <Route path='/orders' element={<Orders></Orders>}></Route>
-        <Route path='/inventory' element={<Inventory></Inventory>}></Route>
+        <Route path='/inventory' element={
+           <RequireAuth>
+                <Inventory></Inventory>
+           </RequireAuth>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path="/login" element={<LogIn></LogIn>}></Route>
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
       </Routes>
     </div>
   );
--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -3,6 +3,12 @@
  * 2.npm install firebase
  * 3.create firebase.init.js and import getAuth to export auth
  * 4.firebase settings > Authentication > enable email and password auth
+ * 5. create login,signup component and set route
+ * 6. attach form field handler and form submit handler
+ * 7.npm install --save react-firebase-hooks
+ * 8.useSignInWithEmailAndPassword  from react-firebase-hooks
+ * 9.if user is created redirect to the expected page
+ * 10.useSignInWithEmailAndPassword for sign in
  */
 
 
